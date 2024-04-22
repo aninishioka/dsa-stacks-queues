6f3d972a4cc6a7ca9d6c6c90de2602641c7a8554
Anissa Nishioka <ani.nishioka@gmail.com>
2024-04-22 16:28:21 -0700

    implement queueStr, stackStr, string-reversal

--- stacks-queues/queue-str/queueStr.ts
+++ stacks-queues/queue-str/queueStr.ts
@@ -1,28 +1,33 @@
+import { LL } from "../common/ll";
+
 /** QueueStr: can remove from start or add to end */
 
 class QueueStr {
+  _ll : LL<String> = new LL();
 
   constructor(initial: string[] = []) {
+    this._ll = new LL(initial);
   }
 
   /** enqueue(val): add val to end. Returns undefined. */
   enqueue(val: string): void {
+    this._ll.push(val);
   }
 
   /** dequeue(): remove & return item from start.
    * Throws error if empty. */
   dequeue(): string {
-    return "x";
+    return String(this._ll.shift());
   }
 
   /** peek(): return the value of top. */
   peek(): string {
-    return "x";
+    return String(this._ll.getAt(0));
   }
 
   /** isEmpty(): is the queue empty? */
   isEmpty(): boolean {
-    return false;
+    return this._ll.length === 0;
   }
 }
 
--- stacks-queues/stack-str/stackStr.ts
+++ stacks-queues/stack-str/stackStr.ts
@@ -3,30 +3,37 @@ import { IndexError } from "../common/ll";
 /** StackStr: can remove from the top or add to the top. */
 
 class StackStr {
+  _array: string[];
 
   constructor(initial: string[] = []) {
+    this._array = initial;
   }
 
   /** push(val): add val to top. Returns undefined. */
   push(val: string): void {
+    this._array.push(val);
   }
 
   /** pop(): remove and return item from top.
    * Throws error if empty. */
   pop(): string {
-    return "x";
+    if (this._array.length === 0) throw new Error();
+
+    const returnVal = this._array.pop();
+    return returnVal!;
   }
 
   /** peek(): return the value of first item. */
-  peek():string {
-    return "x";
+  peek(): string {
+    if (this._array.length === 0) throw new Error();
+
+    return this._array[this._array.length - 1];
   }
 
   /** isEmpty(): is the stack empty? */
-  isEmpty():boolean {
-    return false;
+  isEmpty(): boolean {
+    return this._array.length === 0;
   }
 }
-
 
 export { StackStr };
